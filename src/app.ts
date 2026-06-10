0610c521392c35b7933588404fefadfc1f747c54
SandritaLopez <sandra.lopez.code@gmail.com>
2026-06-10 12:19:45 -0300

    feat: serve demo/index.html as static files at /demo

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './config/swagger';
 
@@ -13,6 +14,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/demo', express.static(path.join(__dirname, '../demo')));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
